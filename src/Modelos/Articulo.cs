a1f374d8b5800cd1741e5ce46c969bb42cb76bf6
Rocio367 <82185773+Rocio367@users.noreply.github.com>
2021-05-01 15:13:29 -0300

    Clases

---
+++ src/Modelos/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class Articulo
+    {
+		public int IdArticulo { set; get; }
+		public string Codigo { set; get; }
+		public string Descripcion { set; get; }
+		public DateTime FechaCreacion { set; get; }
+		public DateTime FechaModifica{ set; get; }
+		public DateTime FechaBorrado { set; get; }
+
+		public Usuario CreadoPor { set; get; }
+		public Usuario ModificadoPor { set; get; }
+		public Usuario BorradoPor { set; get; }
+
+	}
+}
