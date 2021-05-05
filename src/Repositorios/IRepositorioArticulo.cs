ff7e4483f358e84c68e30551d7e9926dcd41c195
Rocio367 <82185773+Rocio367@users.noreply.github.com>
2021-05-05 20:38:40 -0300

    Servicios para articulo

---
+++ src/Repositorios/IRepositorioArticulo.cs
@@ -0,0 +1,18 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    public interface IRepositorioArticulo
+    {
+        void Guardar(Articulo articulo);
+        void Editar(Articulo articulo);
+        void Eliminar(Articulo articulo);
+
+        List<Articulo> ObtenerArticulos();
+    }
+}
