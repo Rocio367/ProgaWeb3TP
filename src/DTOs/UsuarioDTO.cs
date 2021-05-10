2250180f568c49c664c4e38640b3a7f52204c0c3
AndreaCallejas <fabi_acn@hotmail.com>
2021-05-10 03:04:49 -0300

    validacion de usuario

---
+++ src/DTOs/UsuarioDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DTOs
+{
+
+	public class UsuarioDTO
+
+	{
+		
+	       [Required(ErrorMessage = "Ingrese un valor al campo email")]
+			[EmailAddress(ErrorMessage = "Ingrese un correo electrónico valido")]
+			public string Email { get; set; }
+			[Required(ErrorMessage = "Ingrese un valor al campo contraseña")]
+			public string Password { get; set; }
+
+		
+	}
+}
