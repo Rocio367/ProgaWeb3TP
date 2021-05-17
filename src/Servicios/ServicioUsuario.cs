04669515eaa8991689adb05e902ffd9eda7c78eb
Jose Machicado <Josemiguelmc_1998@hotmail.com>
2021-05-17 00:10:44 -0300

    creando repositorio servicio de usuarios para hacer el abm en memoria

--- src/Servicios/ServicioUsuario.cs
+++ src/Servicios/ServicioUsuario.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using DTOs;
+using Modelos;
+using Repositorios;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Servicios
 {
-    class ServicioUsuario: IServicioUsuario
+    public class ServicioUsuario : IServicioUsuario
     {
+        private IRepositorioUsuario _repositorioUsuario;
+
+        public ServicioUsuario(IRepositorioUsuario repositorioUsuario)
+        {
+            _repositorioUsuario = repositorioUsuario;
+        }
+
+        public List<UsuarioDTO> ObtenerUsuarios()
+        {
+            List<Usuario> usuarios = _repositorioUsuario.ObtenerUsuarios();
+            return usuarios.Select(usuario=> ConvertirEnDTO(usuario)).ToList();
+        }
+
+        private static UsuarioDTO ConvertirEnDTO(Usuario usuario)
+        {
+            return new UsuarioDTO
+            {   IdUsuario = usuario.IdUsuario,
+                EsAdmin = usuario.EsAdmin,
+                Nombre = usuario.Nombre,
+                Apellido = usuario.Apellido,
+                FechaNacimiento = usuario.FechaNacimiento,
+                Email = usuario.Email,
+                Password = usuario.Password
+            };
+        }
     }
 }
