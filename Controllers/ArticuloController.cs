9b14cbc68aedb3fde29b359ed59d5cd1c6d3245b
Rocio Isaurralde <rocio.isau@gmail.com>
2021-06-12 13:32:06 -0300

    Revert "servicio y repositorio pedido"
    This reverts commit 0cec8b623a4726652d1a703ef26e2c7b6c31fdb0.

--- Controllers/ArticuloController.cs
+++ Controllers/ArticuloController.cs
@@ -7,10 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using PagedList;
 using ProgaWeb3TP.Models;
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 namespace ProgaWeb3TP.Controllers
 {
--- src/Servicios/ServicioPedido.cs
+++ src/Servicios/ServicioPedido.cs
@@ -1,12 +1,4 @@
-<<<<<<< Updated upstream
 ﻿using System;
-=======
-﻿
-using DTOs;
-using ProgaWeb3TP.ContextBD;
-using Repositorios;
-using System;
->>>>>>> Stashed changes
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,55 +8,5 @@ namespace Servicios
 {
     class ServicioPedido : IServicioPedido
     {
-<<<<<<< Updated upstream
-=======
-        private IRepositorioPedido _repositorioPedido;
-        public ServicioPedido(IRepositorioPedido repositorioPedido)
-        {
-            _repositorioPedido = repositorioPedido;
-        }
-
-        public List<EstadoPedidoDTO> ObtenerEstados() {
-            List<EstadoPedido> estados = _repositorioPedido.ObtenerEstados();
-            return estados.Select(estado =>
-                new EstadoPedidoDTO
-                {
-                    IdEstadoPedido = estado.IdEstadoPedido,
-                    Descripcion = estado.Descripcion
-                 }).ToList();
-        }
-        public List<ClienteDTO> ObtenerClientes() {
-            List<Cliente> clientes = _repositorioPedido.ObtenerClientes();
-            return clientes.Select(cliente =>
-                new ClienteDTO { 
-                       Nombre=cliente.Nombre,
-                       IdCliente =cliente.IdCliente
-      
-                }).ToList();
-        }
-        public List<PedidoDTO> ObtenerPedidosSinFiltro() {
-            List<Pedido> pedidos = _repositorioPedido.ObtenerPedidosSinFiltro();
-            return pedidos.Select(pedido =>
-                new PedidoDTO
-                {
-                    IdPedido = pedido.IdPedido,
-                    Pedido = pedido.NroPedido.ToString(),
-                    Estado = pedido.IdEstado.ToString(),
-                    UltModificacion = (pedido.FechaModificacion == DateTime.MinValue) ? pedido.FechaModificacion: pedido.FechaModificacion,
-                }).ToList();
-        }
-        public List<PedidoDTO> ObtenerPedidosConFiltro(int id_cliente, int estado, bool eliminados) {
-            List<Pedido> pedidos = _repositorioPedido.ObtenerPedidosConFiltro(id_cliente,estado,eliminados);
-            return pedidos.Select(pedido =>
-                new PedidoDTO
-                {
-                    IdPedido = pedido.IdPedido,
-                    Pedido = pedido.NroPedido.ToString(),
-                    Estado = pedido.IdEstado.ToString(),
-                    UltModificacion = (pedido.FechaModificacion == DateTime.MinValue) ? pedido.FechaModificacion : pedido.FechaModificacion,
-                }).ToList();
-        }
-
->>>>>>> Stashed changes
     }
 }
