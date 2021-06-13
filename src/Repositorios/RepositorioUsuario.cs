36e7d637842e6d41afefc3d9f880c0cba416b4c1
Jose Machicado <Josemiguelmc_1998@hotmail.com>
2021-06-13 17:02:12 -0300

    listar usuario repositorio

---
+++ src/Repositorios/RepositorioUsuario.cs
@@ -0,0 +1,33 @@
+﻿using ProgaWeb3TP.src.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    class RepositorioUsuario : IRepositorioUsuario
+    {
+
+        private _20211CTPContext _contexto;
+        public RepositorioUsuario(_20211CTPContext contexto)
+        {
+            _contexto = contexto;
+        }
+        public void Guardar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObtenerUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> ObtenerUsuarios()
+        {
+            return _contexto.Usuarios.ToList();
+        }
+    }
+}
